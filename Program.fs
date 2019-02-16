f7c6760bce53985cf8bfa83de1c55ff3726463e2
shinkwhek <klklpshin@gmail.com>
2019-02-16 16:29:31 +0900

    Vector

--- Program.fs
+++ Program.fs
@@ -1,14 +1,26 @@
 ﻿open System
 open System.IO
 
-type V =
-    struct
-        val X: float
-        val Y: float
-        val Z: float
-        new(v: float) = {X = v; Y = v; Z = v}
-        new(x: float, y: float, z: float) = {X = x; Y = y; Z = z}
-    end
+type V(x: float, y: float, z: float) = 
+        member this.X = x
+        member this.Y = y
+        member this.Z = z
+        static member ( + )(l: V, r: V) =
+            V(l.X + r.X, l.Y + r.Y, l.Z + r.Z)
+        static member ( - )(l: V, r: V) =
+            V(l.X - r.X, l.Y - r.Y, l.Z - r.Z)
+        static member ( * )(l: V, r: V) =
+            V(l.X * r.X, l.Y * r.Y, l.Z * r.Z)
+        static member ( / )(l: V, r: float) =
+            V(l.X / r, l.Y / r, l.Z / r)
+        static member  minus(v: V) =
+            V(-v.X, -v.Y, -v.Z)
+        static member dot(a: V, b: V) =
+            a.X * b.X + a.Y * b.Y + a.Z * b.Z
+        static member cross(a: V, b: V) =
+            V(a.Y * b.Z - a.Z * b.Y, a.Z * b.X - a.X * b.Z, a.X * b.Y - a.Y * b.X)
+        static member normarize(v: V) =
+            v / sqrt(V.dot(v, v))
 
 [<EntryPoint>]
 let main argv =
