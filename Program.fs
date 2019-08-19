adf366f54055a442feea15830053055468ec66f3
shinkwhek <klklpshin@gmail.com>
2019-08-20 00:22:26 +0900

    fix ppm output

--- Program.fs
+++ Program.fs
@@ -3,24 +3,36 @@ open System.IO
 open Vec
 open Ray
 
+let color(r: Ray) : Vec =
+    let a = r.Direction.Normarize
+    let t = 0.5 * (a.Y + 1.)
+    Vec3(1.,1.,1.) * (1.-t) + Vec3(0.5, 0.7, 1.) * t
 
 [<EntryPoint>]
 let main argv =
     let w = 200
     let h = 100
     let body = seq {
-        yield "P3\n"
+        yield "P3"
         yield (string w) + " " + (string h)
-        yield "\n255\n"
+        yield "255"
         
-        for k in [0..h] do
+        let lowerLeftCorner = Vec3(-2., -1., -1.)
+        let horizontal = Vec3(4., 0., 0.)
+        let vertical = Vec3(0., 2., 0.)
+        let origin = Vec3(0., 0., 0.)
+
+        for k in List.rev [0..h] do
         for i in [0..w] do
-            let col = Vec3((float i) % float w, (float (h-k)) % float h, 0.2)
-            let ir = 255.99 * col.X
-            let ig = 255.99 * col.Y
-            let ib = 255.99 * col.Z
+            let u = (float i) / float w
+            let v = (float k) / float h
+            let r = Ray3(origin, lowerLeftCorner + horizontal*u + vertical*v)
+            let col = color(r)
+            let ir = int(255.99 * col.X)
+            let ig = int(255.99 * col.Y)
+            let ib = int(255.99 * col.Z)
             
-            yield (string ir) + " " + (string ig) + " " + (string ib) + "\n"
+            yield (string ir) + " " + (string ig) + " " + (string ib)
     }
     File.WriteAllLines (@"./result.ppm", body) |> ignore
     0 // return an integer exit code
